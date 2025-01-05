bc817f88f8949b822e661010b761b96e60c9f74c
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-05 11:14:22 -0500

    Create Group the People Given the Group Size They Belong To.py

---
+++ Problems/Group the People Given the Group Size They Belong To.py
@@ -0,0 +1,13 @@
+# Adds dict of lists, appending each group until the length matches the size and appending to result
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        res = []
+        groups = defaultdict(list)
+
+        for i, size in enumerate(groupSizes):
+            groups[size].append(i)
+            
+            if len(groups[size]) == size:
+                res.append(groups.pop(size))
+        
+        return res
