508ff7e8700fbda70f5eb9f658a39f59d3fce80a
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-05 11:11:33 -0500

    Create Decode the Message.py

---
+++ Problems/Decode the Message.py
@@ -0,0 +1,17 @@
+# Use dict to map chars and loop through message to decode through map
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        mapping = {' ': ' '}
+        i = 0
+        res = ''
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        
+        for char in key:
+            if char not in mapping:
+                mapping[char] = letters[i]
+                i += 1
+        
+        for char in message:
+            res += mapping[char]
+                
+        return res
