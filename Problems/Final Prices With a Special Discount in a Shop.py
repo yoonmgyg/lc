24a5a918e6d4c1430793ec8e451f14a5182be79d
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-09 11:19:48 -0500

    Create Final Prices With a Special Discount in a Shop.py

---
+++ Problems/Final Prices With a Special Discount in a Shop.py
@@ -0,0 +1,9 @@
+# Monotonic stack is used to get discount by finding the next smaller element
+class Solution:from 
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        for i in range(len(prices)):
+            while stack and (prices[stack[-1]] >= prices[i]):
+                prices[stack.pop()] -= prices[i]
+            stack.append(i)
+        return prices
