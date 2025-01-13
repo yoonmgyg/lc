93c70af2272b9e03b75e86ec8e78f1402ed6bbcf
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-13 13:05:37 -0500

    Create All Paths From Source to Target.py

---
+++ Problems/All Paths From Source to Target.py
@@ -0,0 +1,16 @@
+# Gets all paths through BFS which adds current path if results in target
+class Solution:
+     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+            q = [[0]]
+            result = []
+            target = len(graph) - 1
+       
+            while q:
+                temp = q.pop(0)
+                if temp[-1] == target:
+                    result.append(temp)
+                else:
+                    for neighbor in graph[temp[-1]]:
+                        q.append(temp + [neighbor])
+            
+            return result
