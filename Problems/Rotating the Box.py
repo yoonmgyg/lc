3eca86b1dc0d6a96959ab1c023bfdaa7376a8766
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-14 12:07:45 -0500

    Create Rotating the Box.py

---
+++ Problems/Rotating the Box.py
@@ -0,0 +1,15 @@
+# Rotates the box by scanning right to left and updating stone if finding # or *
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        for row in box:
+            dropPos = len(row) - 1  
+            
+            for currPos in range(len(row)-1, -1, -1):
+                if row[currPos] == "*":  
+                    dropPos = currPos - 1
+                elif row[currPos] == "#":  
+                    row[dropPos], row[currPos] = row[currPos], row[dropPos]
+                    dropPos -= 1
+
+        rotatedBox = zip(*box[::-1])
+        return rotatedBox
