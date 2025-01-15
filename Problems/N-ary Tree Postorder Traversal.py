9292c0fcea100c20873b16ef3eb517db46cc3719
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-15 11:15:41 -0500

    Create N-ary Tree Postorder Traversal.py

---
+++ Problems/N-ary Tree Postorder Traversal.py
@@ -0,0 +1,15 @@
+# Postorder traversal through a dfs of all children
+class Solution(object):
+    def postorder(self, root):
+        if not root:
+            return []
+
+        res = []
+
+        def dfs(root):
+            for x in root.children:
+                dfs(x)
+            res.append(root.val)
+
+        dfs(root)
+        return res
