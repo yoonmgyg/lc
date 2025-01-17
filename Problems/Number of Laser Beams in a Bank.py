9af02fba9da3a2a0122973af9c5d472eda115a3d
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-17 12:16:09 -0500

    Create Number of Laser Beams in a Bank.py

---
+++ Problems/Number of Laser Beams in a Bank.py
@@ -0,0 +1,11 @@
+# Greedy algorithm to count ones and multiply with previous number
+class Solution:
+    def numberOfBeams(self, bank):
+        ans, temp = 0, 0
+        for s in bank:
+            n = s.count('1')
+            if n == 0:
+                continue
+            ans += temp * n
+            temp = n
+        return ans
