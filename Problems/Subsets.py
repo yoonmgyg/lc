f17b4a619ef2072c4d5db781e4c95696fd7e2465
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-18 12:16:03 -0500

    Create Subsets.py

---
+++ Problems/Subsets.py
@@ -0,0 +1,19 @@
+# Recursively creates subsets by using stack to loop through possible subsets
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def create_subset(i):
+            if i == len(nums):
+                res.append(subset[:])
+                return
+            
+            subset.append(nums[i])
+            create_subset(i+1)
+
+            subset.pop()
+            create_subset(i+1)
+
+        create_subset(0)
+        return res
