ba63922c25a6c64109c056bda2f30861d60cd0fa
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-19 10:34:33 -0500

    Create Minimum Number of Changes to Make Binary String Beautiful.py

---
+++ Problems/Minimum Number of Changes to Make Binary String Beautiful.py
@@ -0,0 +1,10 @@
+# Loop through each strin pair and add to result if they are not equal
+class Solution:
+    def minChanges(self, s: str) -> int:
+        changes = 0
+        
+        for i in range(0, len(s), 2):
+            if s[i] != s[i + 1]:
+                changes += 1
+                
+        return changes
