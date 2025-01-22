cc62e468c137ff35fd13870f725677cdac3f3e6a
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-22 12:34:30 -0500

    Create Longest Common Prefix.py

---
+++ Problems/Longest Common Prefix.py
@@ -0,0 +1,15 @@
+# Loop through string, getting largest prefix and subtracting if not matching string
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pref = strs[0]
+        pref_len = len(pref)
+
+        for s in strs[1:]:
+            while pref != s[0:pref_len]:
+                pref_len -= 1
+                if pref_len == 0:
+                    return ""
+                
+                pref = pref[0:pref_len]
+        
+        return pref
