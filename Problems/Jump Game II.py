46ba746b04b33c84801cedb9ff3337064704550a
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-23 12:27:31 -0500

    Create Jump Game II.py

---
+++ Problems/Jump Game II.py
@@ -0,0 +1,15 @@
+# Use 2 pointers to get near and far position and keep track of farthest position reached 
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        near = far = jumps = 0
+
+        while far < len(nums) - 1:
+            farthest = 0
+            for i in range(near, far + 1):
+                farthest = max(farthest, i + nums[i])
+            
+            near = far + 1
+            far = farthest
+            jumps += 1
+        
+        return jumps
