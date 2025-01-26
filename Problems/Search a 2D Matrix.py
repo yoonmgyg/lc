b6f5c770b6a56380870fad29b3ae17e36689bff3
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-26 13:24:54 -0500

    Create Search a 2D Matrix.py

---
+++ Problems/Search a 2D Matrix.py
@@ -0,0 +1,19 @@
+# Use binary search to get row and column of a guess and find the target
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:        
+        rows, cols = len(matrix), len(matrix[0])
+        left, right = 0, rows * cols - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            row, col = mid // cols, mid % cols
+            guess = matrix[row][col]
+
+            if guess == target:
+                return True
+            elif guess < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
