d28ad6d6504e17e89a65f4940cc563d661e30583
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-01-26 13:23:48 -0500

    Create Search Insert Position.py

---
+++ Problems/Search Insert Position.py
@@ -0,0 +1,17 @@
+# Use binary search to search through list
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
