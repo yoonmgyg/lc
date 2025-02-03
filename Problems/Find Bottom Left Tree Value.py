ec2be904aa2df741fb42d7cf3c5484d856cc512c
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-03 14:06:11 -0500

    Create Find Bottom Left Tree Value.py

---
+++ Problems/Find Bottom Left Tree Value.py
@@ -0,0 +1,18 @@
+# BFS with queue to loop through tree and update leftmost values
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        queue = deque([root])
+        leftmost_value = None
+
+        while queue:
+            node = queue.popleft()
+
+            leftmost_value = node.val
+
+            if node.right:
+                queue.append(node.right)
+            if node.left:
+                queue.append(node.left)
+
+        return leftmost_value
+
