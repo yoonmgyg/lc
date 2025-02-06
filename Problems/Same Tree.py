e546cc149eb513f48bfc8a635d47d923617e8dfd
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-06 11:53:57 -0500

    Create Same Tree.py

---
+++ Problems/Same Tree.py
@@ -0,0 +1,10 @@
+# Recursively check children to be equal
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return False
