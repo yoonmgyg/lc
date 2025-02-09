9ffdac1e937762ed3b3c30df95210e8c15a54edc
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-09 12:17:31 -0500

    Create Count Numbers with Unique Digits.py

---
+++ Problems/Count Numbers with Unique Digits.py
@@ -0,0 +1,12 @@
+# Loop through digits and find unique through DP
+class Solution:
+    def countNumbersWithUniqueDigits(self, n):
+        dp = [1, 10]
+        for i in range(2, n+1):
+            tmp = 81
+            for j in range(1, i-1):
+                tmp *= (9 - j)
+            ans = dp[i-1] + tmp
+            dp.append(ans)
+        
+        return dp[n]
