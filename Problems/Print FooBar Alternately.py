d05ad475fc48a11e1264b3f93d75b84911195906
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-10 10:53:35 -0500

    Create Print FooBar Alternately.py

---
+++ Problems/Print FooBar Alternately.py
@@ -0,0 +1,15 @@
+# Use barrier to make functions wait for each other
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.barrier = Barrier(2)
+
+    def foo(self, printFoo):
+        for i in range(self.n):
+            printFoo()
+            self.barrier.wait()
+
+    def bar(self, printBar):
+        for i in range(self.n):
+            self.barrier.wait()
+            printBar()
