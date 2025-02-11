e65921f52d2501584d7ad2d68294a045a70fff94
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-11 13:18:09 -0500

    Create Flipping an Image.py

---
+++ Problems/Flipping an Image.py
@@ -0,0 +1,8 @@
+# Reverse each element of image and invert value
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        res = []
+        for i in image:
+            i.reverse()
+            res.append([x ^ 1 for x in i])
+        return res
