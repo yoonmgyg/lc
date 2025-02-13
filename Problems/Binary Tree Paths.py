4d8c315f15a36563026a3eef8366642954dfb9e0
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-13 11:58:51 -0500

    Create Binary Tree Paths.py

---
+++ Problems/Binary Tree Paths.py
@@ -0,0 +1,15 @@
+# Use recursive dfs to get paths through tree
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        ans = []
+        def solve(root, s):
+            s += str(root.val)
+            if not root.right and not root.left:
+                ans.append(s)
+                return
+            if root.right:
+                solve(root.right, s+"->")
+            if root.left:
+                solve(root.left, s+"->")
+        solve(root, "")
+        return ans
