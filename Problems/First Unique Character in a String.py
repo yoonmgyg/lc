7aa7976ee55da73acd7865084a911e6840bc047f
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-16 11:00:00 -0500

    Create First Unique Character in a String.py

---
+++ Problems/First Unique Character in a String.py
@@ -0,0 +1,13 @@
+# Use dict as map to get frequency and get first with 1
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        mp = {}
+
+        for a in s:
+            mp[a] = mp.get(a, 0) + 1
+
+        for i in range(len(s)):
+            if mp[s[i]] == 1:
+                return i
+
+        return -1
