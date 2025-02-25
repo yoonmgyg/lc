a14b0f86044ff4d60f5eb48b728de95a96682052
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-25 11:27:44 -0500

    Create Summary Ranges.py

---
+++ Problems/Summary Ranges.py
@@ -0,0 +1,16 @@
+class Solution:
+    def summaryRanges(self, nums):
+        result = []
+        if not nums:
+            return result
+
+        start = nums[0]
+        for i in range(1, len(nums) + 1):
+            if i == len(nums) or nums[i] != nums[i - 1] + 1:
+                if start == nums[i - 1]:
+                    result.append(str(start))
+                else:
+                    result.append(f"{start}->{nums[i - 1]}")
+                if i < len(nums):
+                    start = nums[i]
+        return result
