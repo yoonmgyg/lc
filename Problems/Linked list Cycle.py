952c73399362dbc1f45da6eb3f80c9e818c13ed8
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-26 10:32:56 -0500

    Create Linked list Cycle.py

---
+++ Problems/Linked list Cycle.py
@@ -0,0 +1,14 @@
+# Use fast pointer and check if slow and fast match
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+            if fast == slow:
+                return True
+    
+        return False
