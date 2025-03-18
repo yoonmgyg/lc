cf60329426aa077a43fb7002cc9d4f5284e03c32
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-03-18 12:06:51 -0400

    Create Reverse Linked List.py

---
+++ Problems/Reverse Linked List.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = None
+
+        while head:
+            temp = head.next
+            head.next = node
+            node = head
+            head = temp
+        
+        return node
