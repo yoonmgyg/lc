3efae44a2d1880db13a3085fe10712aa3ddcc981
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-03-20 11:48:57 -0400

    Create Minimum Absolute Difference in BST.py

---
+++ Problems/Minimum Absolute Difference in BST.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        cur, stack, minDiff, prev = root, [], 10**5, -10**5
+        
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            node = stack.pop()
+            minDiff = min(minDiff, node.val - prev)
+            prev = node.val
+            cur = node.right
+        
+        return minDiff
