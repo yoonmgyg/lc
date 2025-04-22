d2f66c2b8e6a61e0c7e462fdd9ef9de130006abc
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-04-22 10:31:19 -0400

    Create Count Pairs Whose Sum is Less than Target.py

---
+++ Problems/Count Pairs Whose Sum is Less than Target.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countPairs(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort() 
+        count = 0 
+        left = 0 
+        right = len(nums)-1 #
+        while (left < right): 
+            if (nums[left] + nums[right] < target): 
+                count += right-left 
+                left += 1 
+            else: 
+                right -= 1 
+        return count 
