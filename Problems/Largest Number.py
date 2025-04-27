2a8d6b5538a7dc65b3ebea0d905830c50f009be1
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-04-27 10:45:21 -0400

    Create Largest Number.py

---
+++ Problems/Largest Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        num_strs = list(map(str, nums))
+        
+        def compare(x, y):
+            if x + y > y + x:
+                return -1  # x should come before y
+            elif x + y < y + x:
+                return 1   # y should come before x
+            else:
+                return 0   # they are equal in terms of ordering
+        
+        num_strs.sort(key=cmp_to_key(compare
+        largest_num = ''.join(num_strs)
+        return '0' if largest_num[0] == '0' else largest_num
