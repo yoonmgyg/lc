220cfb3a3d1c9f7c72df52246270a116b5ff91dd
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-05-20 10:38:11 -0400

    Create Groups of Special-Equivalent Strings.py

---
+++ Problems/Groups of Special-Equivalent Strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numSpecialEquivGroups(self, A: List[str]) -> int:
+        res = set()
+        for s in A:
+            sort_odd_even = ''.join(sorted(s[1::2]) + sorted(s[::2]))
+            res.add(sort_odd_even)
+        return len(res)
