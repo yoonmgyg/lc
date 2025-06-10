58c68b9f0cb95290272a792cd8fcd05981f9cfe5
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-06-10 11:39:14 -0400

    Create Minimum Absolute Difference.py

---
+++ Problems/Minimum Absolute Difference.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        min_diff = float('inf')
+        result = []
+        for i in range(1, len(arr)):
+            min_diff = min(min_diff, arr[i] - arr[i - 1])
+          
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i - 1] == min_diff:
+                result.append([arr[i - 1], arr[i]])
+
+        return result
