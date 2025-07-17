172a510a3bc2ae65ca7bfa0a8d9f238bf74eacad
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-07-17 12:16:24 -0400

    Create Type of Triangle.cpp

---
+++ Problems/Type of Triangle.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if (!isTriangle(nums)) return "none";
+        if (isEquilateral(nums)) return "equilateral";
+        if (isIsosceles(nums)) return "isosceles";
+        return "scalene";
+    }
+
+private:
+    bool isTriangle(const vector<int>& nums) {
+        return (nums[0] + nums[1] > nums[2] &&
+                nums[0] + nums[2] > nums[1] &&
+                nums[1] + nums[2] > nums[0]);
+    }
+
+    bool isEquilateral(const vector<int>& nums) {
+        return nums[0] == nums[1] && nums[1] == nums[2];
+    }
+
+    bool isIsosceles(const vector<int>& nums) {
+        return nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2];
+    }
+};
