1fcc323c0ee43602c92dc0f32d6b22085b192d7f
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-08-06 16:07:36 -0400

    Create Can Attend Meeting.py

---
+++ Problems/Can Attend Meeting.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]):
+        if not intervals:
+            return True
+        intervals.sort(key = lambda x : x[0])
+
+        for i in range(1, len(intervals)):
+            if (intervals[i][0] < intervals[i-1][1]):
+                return False
+            
+        return True
+            
