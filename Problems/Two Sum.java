ff3396a146af9246791ea17c6c5f531a6a3c09c2
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-08-14 18:53:30 -0400

    Update Two Sum.java

--- Problems/Two Sum.java
+++ Problems/Two Sum.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> pairIdx = new HashMap<>();
+        Map<Integer, Integer> pairIdx = new HashMap<>(); // create map to store values
 
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            if (pairIdx.containsKey(target - num)) {
+            if (pairIdx.containsKey(target - num)) { // subtract each number from target and see if value is present in map
                 return new int[] { i, pairIdx.get(target - num) };
             }
             pairIdx.put(num, i);
