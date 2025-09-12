4111122caba2a4a3424f6891c347d2a3d85baec3
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-09-12 11:36:33 -0400

    Create Diameter of Binary Tree.java

---
+++ Problems/Diameter of Binary Tree.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] res = new int[1];
+        dfs(root, res);
+        return res[0];
+    }
+
+    private int dfs(TreeNode root, int[] res) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left, res);
+        int right = dfs(root.right, res);
+        res[0] = Math.max(res[0], left + right);
+        return 1 + Math.max(left, right);
+    }
+}
