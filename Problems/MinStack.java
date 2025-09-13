8a627053877af0ac1f9e7f1d7432068f9e4ee1fe
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-09-13 11:57:33 -0400

    Create MinStack.java

---
+++ Problems/MinStack.java
@@ -0,0 +1,39 @@
+public class MinStack {
+    long min;
+    Stack<Long> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(0L);
+            min = val;
+        } else {
+            stack.push(val - min);
+            if (val < min) min = val;
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+
+        long pop = stack.pop();
+
+        if (pop < 0) min = min - pop;
+    }
+
+    public int top() {
+        long top = stack.peek();
+        if (top > 0) {
+            return (int) (top + min);
+        } else {
+            return (int) min;
+        }
+    }
+
+    public int getMin() {
+        return (int) min;
+    }
+}
