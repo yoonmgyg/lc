469f56b92f619cee2e591154190e8278be7871cc
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-09-14 12:00:55 -0400

    Create Copy Linked List with Random Pointer.java

---
+++ Problems/Copy Linked List with Random Pointer.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Node l1 = head;
+        while (l1 != null) {
+            Node l2 = new Node(l1.val);
+            l2.next = l1.next;
+            l1.next = l2;
+            l1 = l2.next;
+        }
+
+        Node newHead = head.next;
+
+        l1 = head;
+        while (l1 != null) {
+            if (l1.random != null) {
+                l1.next.random = l1.random.next;
+            }
+            l1 = l1.next.next;
+        }
+
+        l1 = head;
+        while (l1 != null) {
+            Node l2 = l1.next;
+            l1.next = l2.next;
+            if (l2.next != null) {
+                l2.next = l2.next.next;
+            }
+            l1 = l1.next;
+        }
+
+        return newHead;
+    }
