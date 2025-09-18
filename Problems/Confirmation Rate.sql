20907094e0ba00e2fa4a8c28fc70665135db5aa0
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-09-18 14:16:23 -0400

    Create Confirmation Rate.sql

---
+++ Problems/Confirmation Rate.sql
@@ -0,0 +1,12 @@
+SELECT s.user_id, ROUND(AVG(
+    CASE
+        WHEN c.action = 'confirmed' THEN 1.00
+        ELSE 0
+    END
+    ),
+2
+) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY user_id;
