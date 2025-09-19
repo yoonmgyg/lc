73f60610b9a06ec877a142b2801a8638ae5f04db
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-09-19 16:09:43 -0400

    Create Jump Game.js

---
+++ Problems/Jump Game.js
@@ -0,0 +1,8 @@
+var canJump = function(nums) {
+    let farthest = 0;
+    for (let i = 0; i < nums.length; ++i) {
+        if (i > farthest) return false;
+        farthest = Math.max(farthest, i + nums[i]);
+    }
+    return true;
+};
