3bb3641e5d812b571879b1386ee43e311cb9b2c9
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-09-19 15:39:11 -0400

    Create Majority Element.js

---
+++ Problems/Majority Element.js
@@ -0,0 +1,16 @@
+var majorityElement = function(nums) {
+    let majority = 0;
+    let count = 0;
+    for (let i = 0; i < nums.length; ++i) {
+        if (count <= 0) {
+            majority = nums[i];
+        } 
+        if (majority == nums[i]) {
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+    return majority;
+};
