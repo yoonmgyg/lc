f7afa4eb385ea9cd828d24efae29717f735e9fc4
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-10-10 11:49:34 -0400

    Create Final String.py

---
+++ Problems/Final String.py
@@ -0,0 +1,15 @@
+class Solution:
+    def finalString(self, s):
+        len_s = len(s)
+        ret_val = ''
+
+        i = 0
+        while (i < len_s):
+            if (s[i] == 'i'):
+                ret_val = ret_val[::-1]
+            else:
+                ret_val += s[i]
+
+            i += 1
+
+        return ret_val
