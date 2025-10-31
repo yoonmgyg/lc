510a3e68a220ac9e0b2ee589c7d953ef3f9b350f
ny <71161998+yoonmgyg@users.noreply.github.com>
2025-10-31 18:21:31 -0400

    Create Rotate.py

---
+++ Problems/Rotate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        
+        k %= len(nums)
+        reverse(0, len(nums) - 1)
+        reverse(0, k - 1)
+        reverse(k, len(nums) - 1)
+        
